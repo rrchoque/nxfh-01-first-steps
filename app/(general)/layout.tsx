bb16e125f15c8e665b126b42a986c39bebafb81a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-04 07:29:24 -0400

    Se crearon nuestras primeras rutas y layouts

---
+++ app/(general)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default function GeneralLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <>
+      <main className="flex flex-col items-center p-24">
+        <span className="text-lg">Hola Mundo</span>
+        { children }
+      </main>
+    </>
+  );
+}
