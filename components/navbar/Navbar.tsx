a68b674e7e001c21a663623f9f2cd53ce4d86ad4
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-05 08:33:29 -0400

    Se modificó el navbar para agregar los enlaces Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,29 @@
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
+
+const navItems = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+]
 
 export const Navbar = () => {
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
 
-      <span>Home</span>
+      <Link href={'/'} className='flex items-center'>
+        <HomeIcon className='mr-2' />
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
       
-      <a className="mr-2" href="/about">About</a>
-      <a className="mr-2" href="/pricing">Pricing</a>
-      <a className="mr-2" href="/contact">Contact</a>
+      {
+        navItems.map( navItem => (
+          <Link key={navItem.path} className="mr-2" href={navItem.path} >{navItem.text}</Link>
+        ))
+      }
     </nav>
   )
 }
